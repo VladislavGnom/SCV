c2b65a58c1f80e6e4c64467658649eba4c3d806d
Vladislav Fatov <vladfatov1054@gmail.com>
2025-05-01 16:25:58 +0300

    improve some tests case

--- quiz/tests/test_forms.py
+++ quiz/tests/test_forms.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from quiz.forms import TestForm
-from quiz.models import Test, Question
+from quiz.models import Test, Question, Answer
 
 
 class TestTestCaseForm(TestCase):
@@ -16,12 +16,32 @@ class TestTestCaseForm(TestCase):
             test=self.test,
             text='Question #2'
         )
+        self.ans1_1 = Answer.objects.create(
+            question=self.q1,
+            text='Correct',
+            is_correct=True,
+        )
+        self.ans1_2 = Answer.objects.create(
+            question=self.q1,
+            text='Incorrect',
+            is_correct=False,
+        )
+        self.ans2_1 = Answer.objects.create(
+            question=self.q2,
+            text='Correct',
+            is_correct=True,
+        )
+        self.ans2_2 = Answer.objects.create(
+            question=self.q2,
+            text='Incorrect',
+            is_correct=False,
+        )
 
 
     def test_valid_form(self):
         form_data = {
-            f'question_{self.q1.pk}': self.q1,
-            f'question_{self.q2.pk}': self.q2,
+            f'question_{self.q1.pk}': self.ans1_1,
+            f'question_{self.q2.pk}': self.ans2_2,
         }
         form = TestForm(data=form_data, questions=self.test.questions.all())
         self.assertTrue(form.is_valid())
